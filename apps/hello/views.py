728b8a39080f0bc93d7a2a85e4eed51f3c97236a
Yauhen Alkhouski <zzreklama@gmail.com>
2017-01-06 17:44:50 +0300

    ticket:1 pass view test

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def contact_detail(request):
+    cd_data = {
+        "first_name": "Yauhen",
+        "last_name": "Alkhouski",
+        "birth_day": "1976-11-20",
+        "bio": "some bio information",
+        "email": "zzreklama@gmail.com",
+        "skype": "i.eugene",
+        "jabber": "iameugene@42cc.co",
+        "other_contacts": "some other contact",
+    }
     template = 'hello/contact_detail.html'
-    return render(request, template, {})
+    return render(request, template, {'cd_data': cd_data})
