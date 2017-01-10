44fb367227f3efb05ed3cf699620951833553167
Yauhen Alkhouski <zzreklama@gmail.com>
2017-01-10 20:41:40 +0300

    ticket:3 hard-coded template

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from apps.hello import views
 
 urlpatterns = patterns(
     '',
     url(r'^$', views.contact_detail, name='contact_detail'),
+    url(r'^requests/$', TemplateView.as_view(
+        template_name='hello/request_list.html'), name="request_list"),
 )
